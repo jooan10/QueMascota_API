ddd1ae7977fb0f10e8cadbcb53784c0af987903a
Joan Llopis Castelló <90603751+jooan10@users.noreply.github.com>
2023-05-28 13:05:58 +0200

    WIP

--- index.js
+++ index.js
@@ -4,21 +4,11 @@ import "./database/connectdb.js";
 import express from "express";
 import authRouter from './routers/auth.route.js';
 
-
 const app = express();
 
 app.use(express.json());
 
-
-app.use(methodOverride(function (req, res) {
-    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-      let method = req.body._method;
-      delete req.body._method;
-      return method;
-    } 
-}));
-
-app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/auth',authRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("📡📡📡 http://localhost:" + PORT));
