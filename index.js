d3e9ec2528e573f9ed952d683c2612bf58cfc197
Joan Llopis Castelló <90603751+jooan10@users.noreply.github.com>
2023-05-28 19:44:57 +0200

    wip

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ import "./database/connectdb.js";
 import express from "express";
 import authRouter from './routers/auth.route.js';
 import users from './routers/user.route.js';
+import pets from './routers/pet.route.js';
+
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/auth', users);
+app.use('/api/v1/users', users);
+app.use('/api/v1/pets', pets);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("📡📡📡 http://localhost:" + PORT));
