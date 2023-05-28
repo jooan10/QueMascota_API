97c1f92d6fe58c4e2c32306c0d9d05dd8cf53f6d
Joan Llopis Castelló <90603751+jooan10@users.noreply.github.com>
2023-05-28 19:38:14 +0200

    wip

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ import "./database/connectdb.js";
 
 import express from "express";
 import authRouter from './routers/auth.route.js';
+import users from './routers/user.route.js';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/auth', users);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("📡📡📡 http://localhost:" + PORT));
