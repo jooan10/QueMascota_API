d08d185cf8cc2041b7702ec06167c788b9f2a2d4
Joan Llopis Castelló <90603751+jooan10@users.noreply.github.com>
2023-05-28 17:58:41 +0200

    wip

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ import "dotenv/config";
 import "./database/connectdb.js";
 
 import express from "express";
-import authRouter from './routers/user.route.js';
+import authRouter from './routers/auth.route.js';
+import user from './routers/user.route.js';
+import pet from './routers/pet.route.js';
+
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/v1/auth',authRouter);
+app.use('/user',user);
+app.use('/pet',pet);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("📡📡📡 http://localhost:" + PORT));
