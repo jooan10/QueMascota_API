356c7a475e4759ba95f7ab87d11cc43bc04c3d3d
Joan Llopis Castelló <90603751+jooan10@users.noreply.github.com>
2023-05-28 13:02:13 +0200

    WIP

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import express from "express";
 import authRouter from './routers/auth.route.js';
 import pet from './routers/pet.route.js';
 
-const app = express();
+let app = express();
 
 app.use(express.json());
 
