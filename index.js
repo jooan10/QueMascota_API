cf62f1e1d6cdcc42c17bb5c29f3e2af53a5d1e96
Joan Llopis Castelló <90603751+jooan10@users.noreply.github.com>
2023-05-28 13:43:33 +0200

    wip

--- index.js
+++ index.js
@@ -7,15 +7,6 @@ import authRouter from './routers/auth.route.js';
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded());
-
-app.use(methodOverride(function (req, res) {
-    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-      let method = req.body._method;
-      delete req.body._method;
-      return method;
-    } 
-}));
 
 app.use('/api/v1/auth',authRouter);
 
