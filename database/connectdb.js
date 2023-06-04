d2bad31b998f14e9636088d50f457327990f6f71
Joan Llopis Castelló <90603751+jooan10@users.noreply.github.com>
2023-06-04 18:20:46 +0200

    WIP

--- database/connectdb.js
+++ database/connectdb.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 try {
     //Con esta URI se podria levantar en local
-    await mongoose.connect('mongodb://localhost:27017/quemascota');
+    //await mongoose.connect('mongodb://localhost:27017/quemascota');
     //Y con esta segunda seria para levantarlo a través del VPS
-    //await mongoose.connect('mongodb://mymongodb/quemascota');
+    await mongoose.connect('mongodb://mymongodb/quemascota');
     console.log("Connect DB ok 👋");
 } catch (error) {
     console.log("Error de conexión a mongodb:" + error);
