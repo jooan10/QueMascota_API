3437f2690fc763c69dfd489f1763f6807928b015
Joan Llopis Castelló <90603751+jooan10@users.noreply.github.com>
2023-06-04 19:55:40 +0200

    wip

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import pets from './routers/pet.route.js';
 const app = express();
 
 app.use(cors())
-app.use(express.json());
+app.use(express.json({limit:"10mb"}));
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', users);
