40286571a53c5badd1aad547199d15417e6a8fa3
Joan Llopis Castelló <90603751+jooan10@users.noreply.github.com>
2023-06-07 22:23:31 +0200

    wip

--- routers/user.route.js
+++ routers/user.route.js
@@ -1,31 +1,12 @@
 import express from "express";
 import { User } from "../models/User.js";
-import fs from "fs";
+
 
 const router = express.Router();
-const apkFilePath = '/downloads/QueMascota.apk';
+
 
 //Servicio para descargar APK
-app.get('/download', (req, res) => {
-    // Verificar si el archivo APK existe
-    if (fs.existsSync(apkFilePath)) {
-      // Obtener el nombre del archivo
-      const fileName = path.basename(apkFilePath);
-      
-      // Establecer las cabeceras de respuesta para la descarga
-      res.setHeader('Content-disposition', 'attachment; filename=' + fileName);
-      res.setHeader('Content-type', 'application/vnd.android.package-archive');
-      
-      // Crear un stream de lectura del archivo APK
-      const fileStream = fs.createReadStream(apkFilePath);
-      
-      // Transmitir el archivo al cliente
-      fileStream.pipe(res);
-    } else {
-      // El archivo APK no existe
-      res.status(404).send('Archivo APK no encontrado.');
-    }
-  });
+
 
 //Servicio GET /Users
 router.get('/', (req, res) => {
